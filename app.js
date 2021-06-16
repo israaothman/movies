6721e2c9bf0e33f40b16fc1ad864bc986eb9decb
israaothman <>
2021-06-16 10:11:45 +0000

    add java script

---
+++ app.js
@@ -0,0 +1,198 @@
+// a comment 
+
+/*
+multi comment
+sjfnjfj
+fnjdnfjd
+sknfksnf
+ksmmk
+
+ */
+
+
+/*
+Syntax
+
+Data types :
+
+- numbers ; 1 , 5 -2 
+
+-boolean : true/false 
+
+- strings : "ahmad", 'aknnadkandka !@# 1233'
+
+variables: 
+
+- to store anything in them 
+
+var name = "ahmad"
+
+var x = 10 
+
+var sum = 0 + x => 0 + 10 =>10 
+
+
+
+
+operators:
+
+arithmatic operators:
+
++ , - , * , /
+
+1+1 
+6*7
+
+assigment operator:
+
+= 
+
+comparison operators:
+
+> , < , == , === , != 
+
+1 > 9 
+
+
+
+
+*/
+
+// var name = "ahmad ' \" 0 1 +1 @ @$ckdsnksdnv "
+
+// console.log(name)
+// var firstName = "Ahlam";
+
+// var x // undefined
+// x = 928
+// var y = 190
+
+// x = 10
+// var sum = x+y
+
+// console.log(x)
+// console.log(sum)
+
+
+// name = "Nahla"
+
+// console.log(name)
+
+
+
+// console.log(firstName)
+
+// console.log("hello world 1+1")
+
+// console.log(1+1)
+
+
+// comparison  ===, check the value and datatype
+
+// console.log(1=='1', 1==='1', 1!=1, 1 < 1, 1<=1)
+
+
+//boolean datatype
+
+// var isGreater = true;
+
+//concatation
+
+// var firstName = "ahmad  "
+// var lastName = "Jihad"
+
+// var name = firstName + lastName + "   wahtever "+ 1
+
+// console.log(name)
+
+
+//conditional statments
+
+/* 
+
+if(condtion){
+  code 
+}
+else{
+  code 
+}
+
+*/
+
+
+// if(10 === 4){
+//   console.log("yes it is")
+// }
+// else{
+//   console.log("no it is not")
+// }
+
+
+
+//if elsif 
+
+
+// if (10 < 8){
+//     console.log("yes it is")
+
+// }
+// else if (20 > 150)
+// {
+//   console.log("20 greater than 15")
+// }
+// else if(100 === 1000) {
+//   console.log("whatever")
+// }
+// else{
+//     console.log("no it is not")
+
+// }
+
+
+/* 
+
+pop up box 
+
+alert() => showing info to user 
+
+
+prompt()= > it will take input and we can assign to a variable 
+
+
+
+*/
+var name = prompt("what is your name?")
+
+alert("welcome to Fantacy movies website " + name)
+
+var favMovie = prompt("what is your fav movies from these (harry potter, fast and furious , forrest gump )?")
+
+
+if(favMovie == "harry potter"){
+  document.write("<p>"+ "great thing you love harry's movies becuase we have it here so you can watch it"+"</p>")
+}
+ else if(favMovie == "fast and furious")
+{
+ document.write("<div>"+"<h3>"+"Harry Potter"+"</h3>"+
+        "<img src='https://www.denofgeek.com/wp-content/uploads/2016/11/harry-potter-movies.png?resize=768%2C432' >"+
+      "</div>"
+ )
+
+
+}
+
+else if(favMovie == "forrest gump")
+{
+ document.write("<p>"+ "great thing you love forrest gump movies becuase we have it here so you can watch it"+"</p>")
+}
+
+else{
+  alert("sorry we don't have this movie")
+}
+
+
+// var num = prompt("guess a number?")
+
+// console.log(typeof(num))
+
+
